0b8be5a0d3917bb74e9753c7f8c1f2af40d39458
Jose Martinez <rafamartz2007@gmail.com>
2026-08-09 18:19:32 -0600

    0.1.0

---
+++ lib/constraints/colors.dart
@@ -0,0 +1,14 @@
+// Color scheme
+
+// Backgrounds
+const String bg = "010101";
+const String bg_container = "0E0E0E";
+const String bg_ghost = "191919";
+
+// Text
+const String text = "FAFAFA";
+const String text_gray = "646464";
+
+// Main colors
+const String primmary = "24E124";
+const String primmary_hover = "1CB31C";
